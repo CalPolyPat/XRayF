43c0b97653bd7bbe4d7b9af02060f41d12ffe407
psteffanic <psteffan@calpoly.edu>
2015-05-01 13:01:57 -0700

    Real Abun

--- Load_Spectrum.py
+++ Load_Spectrum.py
@@ -9,42 +9,49 @@ gROOT.Reset()
 ROOT.gROOT.SetStyle('Plain')
 ROOT.gStyle.SetFillColor(0)
 
-def Load_Spectrum(filename, histname):
-	f = open(filename)
-	workline = ""
-	norm = 0
-	data = []
-	record = False
-	getnorm = True
-	
-	for line in f:
-		j=0
-		workline = ""
-		if getnorm == True:
-			while line[j]!=chr(32):
-				workline = workline+line[j]
-				j+=1
-		if workline == "LIVE_TIME":
-			norm = line[-11:-1]
-			getnorm = False
-			print norm
-		if str(line) == '<<END>>\n':
-			record = False    
-			print "Data feed end"
-		if str(line).startswith('MCAC'):
-			chan=line[5:8]
-			print chan
-		if record == True:
-			data.append(int(line))
-		if str(line) == "<<DATA>>\n":
-			record = True
-			print "Data feed start"
-	for i in xrange(len(data)):
-		data[i]=data[i]/float(norm)
-	
-	spect = ROOT.TH1F("spect", histname, int(chan), 0, float(chan))
-	i = 1
-	for d in data:
-		spect.Fill(i, d)
-		i=i+1	
-	return spect
+def Load_Spectrum(filename, histname, normret=False):
+    f = open(filename)
+    workline = ""
+    norm = 0;
+    n=0
+    data = []
+    record = False
+    getnorm = True
+
+    for line in f:
+        j=0
+        workline = ""
+        if getnorm == True:
+            while line[j]!=chr(32):
+                workline = workline+line[j]
+                j+=1
+            if workline == "LIVE_TIME":
+                norm = line[-11:-1]
+                getnorm = False
+            print norm
+        if str(line) == '<<END>>\n':
+            record = False    
+            print "Data feed end"
+        if str(line).startswith('MCAC'):
+            chan=line[5:8]
+            print chan
+        if record == True:
+            data.append(int(line))
+        if str(line) == "<<DATA>>\n":
+            record = True
+            print "Data feed start"
+    for i in xrange(len(data)):
+        data[i]=data[i]/float(norm)
+
+    spect = ROOT.TH1F("spect", histname, int(chan), 0, float(chan))
+    i = 1
+    for d in data:
+        spect.Fill(i, d)
+        i=i+1
+    normal = 1;
+    n=spect.GetEntries()
+    spect.Scale(normal/spect.Integral("width"))
+    print("It is updating")
+    if normret:
+        return spect, n
+    return spect
